59d77e79e2b8901283b59bd8cc237d8e15cd891b
victor slls <victorslls.com>
2024-01-09 09:44:18 -0300

    refatoração main js

--- main.js
+++ main.js
@@ -1,34 +1,38 @@
 /*  abre e fecha o menu quando clicar no icone: hamburguer e x */
 const nav = document.querySelector('#header nav')
 const toggle = document.querySelectorAll('nav .toggle')
+/* quando clicar em um item do menu, esconder o menu */
+const links = document.querySelectorAll('nav ul li a')
+/* mudar o header da página quando der scroll */
+const header = document.querySelector('#header')
+const navHeight = header.offsetHeight
+/* Botão voltar para o topo */
+const backToTopButton = document.querySelector('.back-to-top')
+/* Menu ativo conforme a seção visível na página */
+const sections = document.querySelectorAll('main section[id]')
+
+
+
 
 for (const element of toggle) {
-  element.addEventListener('click', function () {
+  element.addEventListener('click', () => {
     nav.classList.toggle('show')
+    console.log(element);
   })
 }
 
-/* quando clicar em um item do menu, esconder o menu */
-const links = document.querySelectorAll('nav ul li a')
 
 for (const link of links) {
-  link.addEventListener('click', function () {
+  link.addEventListener('click',  () => {
     nav.classList.remove('show')
   })
 }
 
-/* mudar o header da página quando der scroll */
-const header = document.querySelector('#header')
-const navHeight = header.offsetHeight
 
 function changeHeaderWhenScroll() {
-  if (window.scrollY >= navHeight) {
-    // scroll é maior que a altura do header
-    header.classList.add('scroll')
-  } else {
-    // menor que a altura do header
-    header.classList.remove('scroll')
-  }
+  const scrollGreaterThanHeaderHeight = window.scrollY >= navHeight
+
+  scrollGreaterThanHeaderHeight ? header.classList.add('scroll') : header.classList.remove('scroll')
 }
 
 /* Testimonials carousel slider swiper */
@@ -66,21 +70,16 @@ scrollReveal.reveal(
   { interval: 100 }
 )
 
-/* Botão voltar para o topo */
-const backToTopButton = document.querySelector('.back-to-top')
 
 function backToTop() {
-  if (window.scrollY >= 560) {
-    backToTopButton.classList.add('show')
-  } else {
-    backToTopButton.classList.remove('show')
-  }
+  const screenScroll =  window.scrollY >= 560
+
+  screenScroll ? backToTopButton.classList.add('show') : backToTopButton.classList.remove('show')
 }
 
-/* Menu ativo conforme a seção visível na página */
-const sections = document.querySelectorAll('main section[id]')
 function activateMenuAtCurrentSection() {
   const checkpoint = window.pageYOffset + (window.innerHeight / 8) * 4
+ 
 
   for (const section of sections) {
     const sectionTop = section.offsetTop
